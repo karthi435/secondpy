aade1568e73505c235787eb61c84aa6c14034424
RK435 <>
2021-06-05 18:14:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print(type(2+4))
+print(int(2.5+6.2))
+print(type(2.5+6.2))
+print(2 ** 3)
+print(5 // 4)
+print(6 % 4)
+print(round(3.2))
+
+a = 20 + 3 * 4
+print(a)
+a = (20 - 3) + 2 ** 2
+print(a)
+#() ** * / + -
+
+print(complex(2, 3))
+print(bin(5))
+print(int('0b101', 2))
